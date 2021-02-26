27419478dcb04ec10a23b282f9e896ebb6e3df42
Kirill Prazdnikov <kirill.prazdnikov@jetbrains.com>
2021-02-26 15:40:23 +0300

    clean-up

--- simpleApp/app.cpp
+++ simpleApp/app.cpp
@@ -2,7 +2,7 @@
 
 #include "GraalSample.h"
 
-static graal_isolatethread_t * createGraalVM(__graal_uword gb) {
+static graal_isolatethread_t * createGraalVM() {
   graal_isolatethread_t* thread = 0;
 
   int error = graal_create_isolate(0, 0, &thread);
@@ -16,7 +16,7 @@ static graal_isolatethread_t * createGraalVM(__graal_uword gb) {
 
 
 int main() {
-  graal_isolatethread_t* graalThread = createGraalVM(2);
+  graal_isolatethread_t* graalThread = createGraalVM();
   if (graalThread) {
     int add4 = EntryPoints__add__e294c015aa64461959a1b2cb50ba9cc185789b28(graalThread, 4);
     int sub7 = EntryPoints__sub__f889e49a63b079c60481eb876fa6a0a0575f8f04(graalThread, 7);
